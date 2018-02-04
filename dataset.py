eca6c6b10ea06d211ee483443270c0d2cf718e43
Dusan <dusanix@gmail.com>
2018-02-04 20:36:40 +0100

    Add SimpleCNN, SimpleFCN, augmented patches dataset

--- dataset.py
+++ dataset.py
@@ -3,7 +3,9 @@ import cv2
 import h5py
 import numpy as np
 from random import shuffle
+from skimage import exposure
 from keras.utils import Sequence
+
 # class CIFAR10Sequence(Sequenclass CIFAR10Sequence(Sequence):
 #
 #     def __init__(self, x_set, y_set, batch_size):
@@ -27,7 +29,7 @@ class SinglePatchDataset(Sequence):
         self.h5file = h5py.File(hdf5_filepath, "r")
         self.num_class = 10
         self.batch_size = batch_size
-        self.num = self.h5file["train_patches"].shape[0]
+        self.num = self.h5file["patches"].shape[0]
         print(self.num)
 
     def __len__(self):
@@ -41,9 +43,12 @@ class SinglePatchDataset(Sequence):
     def __getitem__(self, idx):
         start = idx*self.batch_size
         end = (idx+1)*self.batch_size
-        batch_x = self.h5file["train_patches"][start:end, ...]
+        batch_x = self.h5file["patches"][start:end, ...]
         batch_y = self.one_hot(self.h5file["labels"][start:end])
         return batch_x, batch_y
+
+    def on_epoch_end(self):
+        pass
 
 
 class CIDDataset():
@@ -67,22 +72,10 @@ class CIDDataset():
         print(self.x_filenames)
         self.i = 0
 
-    def patches_from_img(self, filepath, num_patches, patch_sz=512):
-        img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)/255
-        img_h, img_w, channels = img.shape
-        patches = np.zeros([num_patches, patch_sz, patch_sz, channels])
-        patch_x_ind = np.random.randint(0, img_w-patch_sz, num_patches,
-                                        dtype=np.int32)
-        patch_y_ind = np.random.randint(0, img_h-patch_sz, num_patches,
-                                        dtype=np.int32)
-        for num in range(num_patches):
-            x, y = patch_x_ind[num], patch_y_ind[num]
-            patches[num, :, :, :] = img[y:y+patch_sz, x:x+patch_sz :]
 
-        return patches
 
     def center_patch(self, filepath, patch_sz=512):
-        img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)/255
         img_h, img_w, channels = img.shape
         x = img_h//2-patch_sz//2
         y = img_w//2-patch_sz//2
@@ -103,12 +96,83 @@ class CIDDataset():
         val_x = shuf_filenames[tend:]
         val_y = shuf_labels[tend:]
 
-        print(len(train_x))
-        print(len(train_y))
-        print(len(val_x))
-        print(len(val_y))
-
         return train_x, train_y, val_x, val_y
+
+    def augment_patch(self, patch, patch_sz, mods):
+        patch_aug = np.zeros([len(mods), patch_sz, patch_sz, 3])
+        ind = 0
+        if "original" in mods:
+            patch_aug[ind, :, :, :] = patch
+            ind += 1
+        if "gamma0.8" in mods:
+            bla = exposure.adjust_gamma(patch, 0.8)
+            patch_aug[ind, :, :, :] = bla
+            ind += 1
+        if "gamma1.2" in mods:
+            patch_aug[ind, :, :, :] = exposure.adjust_gamma(patch, 1.2)
+            ind += 1
+
+        # TODO: Implement other augmentations
+        return patch_aug
+
+
+    def patches_from_img(self, filepath, num_patches, mods, patch_sz=512):
+        num_mods = len(mods)
+        img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)/255
+        img_h, img_w, channels = img.shape
+        patches = np.zeros([num_patches*num_mods, patch_sz, patch_sz, channels])
+        patch_x_ind = np.random.randint(0, img_w-patch_sz, num_patches,
+                                        dtype=np.int32)
+        patch_y_ind = np.random.randint(0, img_h-patch_sz, num_patches,
+                                        dtype=np.int32)
+        for num in range(num_patches):
+            x, y = patch_x_ind[num], patch_y_ind[num]
+            sind = num*num_mods
+            eind = sind+num_mods
+            patches[sind:eind, :, :, :] = img[y:y+patch_sz, x:x+patch_sz :]
+
+        return patches
+
+    def aug_patch_dataset(self, output_folder):
+        patch_sz = 512
+        num_patches = 1
+        mods = ["original"]
+        # mods = ["original", "gamma0.8", "gamma1.2"]
+        newly_gen = len(mods) * num_patches
+
+        train_x, train_y, val_x, val_y = self.shuffle_data()
+        train_shape = (len(train_x)*newly_gen, patch_sz, patch_sz, 3)
+        val_shape = (len(val_x)*newly_gen, patch_sz, patch_sz, 3)
+
+        # Create validation set
+        h5val_file = h5py.File(os.path.join(output_folder, "val.hdf5"))
+        h5val_file.create_dataset("patches", val_shape, np.int8)
+        h5val_file.create_dataset("labels", data=np.array(val_y))
+        for i, filepath in enumerate(val_x):
+            patches = self.patches_from_img(filepath, num_patches, mods)
+            sind = i*newly_gen
+            eind = sind+newly_gen
+            h5val_file["patches"][sind:eind, ...] = patches
+            # patch = self.center_patch(filepath_x)
+            # augs = self.augment_patch(patch, 512, mods)
+            # for i in range(augs.shape[0]):
+            #     cv2.imwrite(str(i)+"img.jpg", augs[i])
+            #patches = self.patches_from_img(filepath_x, 5, mods)
+
+            break
+        h5val_file.close()
+
+        # Create train set
+        h5train_file = h5py.File(os.path.join(output_folder, "train.hdf5"))
+        h5train_file.create_dataset("patches", train_shape, np.int8)
+        h5train_file.create_dataset("labels", data=np.array(train_y))
+        for i, filepath in enumerate(train_x):
+            patches = self.patches_from_img(filepath, num_patches, mods)
+            sind = i*newly_gen
+            eind = sind+newly_gen
+            h5train_file["patches"][sind:eind, ...] = patches
+            break
+        h5train_file.close()
 
     def single_patch_dataset(self, output_folder):
 
@@ -118,10 +182,6 @@ class CIDDataset():
 
         train_shape = (len(train_x), patch_sz, patch_sz, 3)
         val_shape = (len(val_x), patch_sz, patch_sz, 3)
-
-        for a, b in zip(train_x, train_y):
-            print(a, " ", b)
-        exit()
 
         # Create validation set
         h5val_file = h5py.File(os.path.join(output_folder, "val.hdf5"))
@@ -149,7 +209,8 @@ class CIDDataset():
 
 if __name__ == "__main__":
     dataset = CIDDataset("data/vanilla/train")
-    dataset.single_patch_dataset("data/single_patch")
+    dataset.aug_patch_dataset("data/aug_patch")
+    # dataset.single_patch_dataset("data/single_patch")
 
     # a = SinglePatchDataset("data/single_patch/train.hdf5", 16)
     # x, y = a[4]
---
+++ simple_nn.py
@@ -0,0 +1,95 @@
+import os
+
+import keras
+from keras.optimizers import Adam
+from keras.models import Sequential
+from dataset import SinglePatchDataset
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense
+
+
+class SimpleNN():
+    def __init__(self, dataset, val_dataset, model_type):
+        self.data_shape = (512, 512, 3)
+        self.dataset = dataset
+        self.val_dataset = val_dataset
+        self.model_name = ""
+
+        if model_type == "cnn":
+            self.model = self.get_cnn()
+        elif model_type == "fcn":
+            self.model = self.get_fcn()
+        else:
+            print("Model not supported")
+            exit()
+
+    def train(self):
+        os.makedirs("weights", exist_ok=True)
+        weights_filepath = "weights/"+self.model_name+".hdf5"
+
+        if os.path.isfile(weights_filepath):
+            self.model.load_weights(weights_filepath)
+
+        checkpointer = ModelCheckpoint(weights_filepath)
+        rlrop = ReduceLROnPlateau(monitor='val_loss',
+                                  factor=0.2,
+                                  patience=2,
+                                  verbose=1,
+                                  epsilon=1e-5,
+                                  mode='max')
+
+        tensorboard = keras.callbacks.TensorBoard(
+            log_dir='logs/' + self.model_name + '/',
+            histogram_freq=0,
+            write_graph=True,
+            write_images=False)
+
+        self.model.fit_generator(self.dataset, len(self.dataset), epochs=25,
+                                 validation_data=self.val_dataset,
+                                 validation_steps=len(self.val_dataset),
+                                 callbacks=[checkpointer, tensorboard, rlrop])
+
+    def get_cnn(self):
+        self.model_name = "SimpleCNN"
+        model = Sequential()
+        model.add(Convolution2D(32, (5, 5),
+                                input_shape=self.data_shape,
+                                activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Convolution2D(16, (5, 5), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Convolution2D(8, (5, 5), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.2))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(50, activation='relu'))
+        model.add(Dense(10, activation='softmax'))
+        optimizer = Adam(lr=0.0001)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=optimizer,
+                      metrics=['accuracy'])
+        return model
+
+    def get_fcn(self):
+        self.model_name = "SimpleFCN"
+        model = Sequential()
+        model.add(Flatten(input_shape=self.data_shape))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(10, activation='softmax'))
+        optimizer=Adam(lr=0.00001)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=optimizer,
+                      metrics=['accuracy'])
+        return model
+
+    def predict_single(self):
+        pass
+
+
+if __name__ == "__main__":
+    train_gen = SinglePatchDataset("data/single_patch/train.hdf5", 2)
+    val_gen = SinglePatchDataset("data/single_patch/val.hdf5", 2)
+    model = SimpleNN(train_gen, val_gen, "fcn")
+    model.train()
